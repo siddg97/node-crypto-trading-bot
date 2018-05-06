362914267a2ad4d8fd1598f6572ba4fce25d0fb8
vinsonly <vinsonly@live.ca>
2018-05-06 00:13:01 -0700

    changed from node-binance-api to binance package

---
+++ controllers/login.js
@@ -0,0 +1,1 @@
+console.log("started login.js");
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 console.log("App Started.");
+
+const mongoose = require('mongoose');
+
+const db = mongoose.connect('mongodb://127.0.0.1/tradingbot');
