fdf05f09ecf52ac5959fb3000d2cc5f4e9ebb7e6
vinsonly <vinsonly@live.ca>
2018-05-06 23:15:00 -0700

    finished writing code to prompt user to enter info for kline get req

--- controllers/trade_history_downloader.js
+++ controllers/trade_history_downloader.js
@@ -7,63 +7,72 @@ const progress = require('progress');
 const moment = require('moment');
 const readline = require('readline');
 
-const rl = readline.createInterface({
+var rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-function promptUserForInfo() {
-  console.log("Please enter the requested information to continue with your historical data request.");
+
+async function promptUserForInfo() {
 
   var params = {};
 
-  rl.question("symbol (eg, BTCUSD or ETHBTC or ICXBTC):", (answer) => {
-    params.symbol = answer;
-  });
+  var askSymbol = function() {
+    return new Promise((resolve) => {
+      rl.question("symbol (eg, BTCUSD or ETHBTC or ICXBTC): ", (answer) => {
+        resolve();
+        params.symbol = answer;
+        console.log("params.symbol = " + params.symbol);
+      })
+    })
+  }
 
-  rl.question("interval (one of (1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M)):", (answer) => {
-    params.interval = answer;
-  });
+  var askInterval = function() {
+    return new Promise((resolve) => {
+      rl.question("interval (one of (1m, 3m, 5m, 15m, 30m, 1h, 2h, 4h, 6h, 8h, 12h, 1d, 3d, 1w, 1M)): ", function(answer){
+        resolve();
+        params.interval = answer;
+        console.log("params.interval = " + params.interval);
+      })
+    })
+  }
 
-  rl.question("limit (max 500):", (answer) => {
-    params.limit = answer;
-  });
+  var askLimit = function() {
+    return new Promise((resolve) => {
+      rl.question("limit (max 500): ", (answer) => {
+        resolve();
+        params.limit = answer;
+      });
+    })
+  }
 
-  params.startTime = promptTime();
+  async function start() {
+    await askSymbol();
+    await askInterval();
+    await askLimit();
+    params.startTime = await promptTime("start");
+    params.endTime = await promptTime("end");
+    await console.log(params);
+    await rl.close();
+  }
 
-  params.endTime = promptTime();
+  await console.log("Please enter the requested information to continue with your historical data request.");
 
-  console.log("params");
+  await start();
+
 }
 
-function promptTime() {
-  console.log("Enter time in the format of Unix timestamp.");
-  var currentDate = new Date();
-  var startTime;
-  console.log(`eg: ${currentDate.getTime()}`);
-  rl.question("", (answer) => {
-    startTime = answer;
+function promptTime(type) {
+  return new Promise((resolve) => {
+    console.log(`Enter ${type} time in the format of Unix timestamp. `);
+    var currentDate = new Date();
+    var startTime;
+    console.log(`eg: ${currentDate.getTime()}`);
+    rl.question("", (answer) => {
+      startTime = answer;
+      resolve(answer);
+    })
   })
-  // console.log("startTime:");
-  // var startTime = {};
-  // console.log("year:");
-  // startTime.year = readline();
-  // console.log("month:");
-  // startTime.month = readLine();
-  // console.log("day:");
-  // startTime.day = readLine();
-  // console.log("hour:");
-  // startTime.hour = readLine();
-  // console.log("minute:");
-  // startTime.minute = readLine();
-  // console.log("second:");
-  // startTime.second = readLine();
-  // console.log("millisecond:");
-  // startTime.millisecond = readLine();
-  //
-  console.log("time = ");
-  console.log(startTime);
-  return startTime;
 }
 
 module.exports = promptUserForInfo;
