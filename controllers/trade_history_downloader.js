b3a70e96d759dd921cd63ab73d81b15acf3c0142
vinsonly <vinsonly@live.ca>
2018-05-08 23:31:43 -0700

    wrote code to save kline data into file as json form

--- controllers/trade_history_downloader.js
+++ controllers/trade_history_downloader.js
@@ -1,16 +1,17 @@
 const Binance = require('binance-api-node').default
 const progress = require('progress');
 const prompt = require('./promptKlineInfo');
+const fs = require('fs');
+const jsonfile = require('jsonfile');
 
-//ask user for info
+//ask user for inf
 
- 
 const client = Binance();
- 
+
 client.time().then(time => console.log(time))
 
 
-var params = {}; 
+var params = {};
 
 // prompt().then((result) => params = result).catch((error) => console.log(error));
 
@@ -29,7 +30,19 @@ params.endTime = 1522627200000;
 console.log("params = ");
 console.log(params);
 
+var file = '/home/vinsonly/node-crypto-trading-bot/history-files/history.json'
 
-client.candles(params).then( (result) => {console.log(result)}).catch( (error) => {console.log(error)} );
+var jsonResult;
 
-
+client.candles(params)
+.then((result) => {
+  console.log(result);
+  jsonResult = result;
+})
+.then(() => {
+  jsonfile.writeFile(file, jsonResult, (err) => {
+    console.log(err);
+  })
+})
+.then(()=> {console.log("done")})
+.catch( (error) => {console.log(error)} );
