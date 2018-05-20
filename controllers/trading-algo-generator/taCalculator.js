a3dc743a3be4e4689973fe11ce0183259ce887d7
vinsonly <vinsonly@live.ca>
2018-05-20 11:04:30 -0700

    installed technicalindicators node package

--- controllers/trading-algo-generator/taCalculator.js
+++ controllers/trading-algo-generator/taCalculator.js
@@ -1,1 +1,9 @@
 var tulind = require('tulind');
+var talib = require('talib');
+var techind = require('technicalindicators');
+
+function checkVersions() {
+  console.log("tulid: " + );
+  console.log("tallb: " + );
+  console.log("techind: " + );
+}
