21f690664a90c57c954cb163f8909e5537b975c2
vinsonly <vinsonly@live.ca>
2018-06-03 13:34:55 -0700

    insalled talib package

--- controllers/trading-algo-generator/taCalculator.js
+++ controllers/trading-algo-generator/taCalculator.js
@@ -1,9 +1,10 @@
 var techind = require('technicalindicators');
 var tulind = require('tulind');
+var talib = require('talib');
 
 function checkVersions() {
     console.log("tulind version: " + tulind.version);
+    console.log("talib viersion: " + talib.version);
 }
 
 module.exports.checkVersions = checkVersions;
-
